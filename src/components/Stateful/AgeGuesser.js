cedfc5471d4ceedf4b95df88a04a6672b0635b76
Mark Sarkis <msarkis@latchel.com>
2022-01-16 19:36:23 -0800

    added comments

--- src/components/Stateful/AgeGuesser.js
+++ src/components/Stateful/AgeGuesser.js
@@ -10,8 +10,8 @@ class AgeGuesser extends React.Component {
     // keep track of timeout
     timeout = 0;
 
-    // updates the state with any key/value pair passed
-    // event is passed in here, so we can deconstruct
+   // updates the state with any key/value pair passed
+   // event is passed in here, so we can deconstruct
     // inputHandler = (event) => {
         // const name = event.target.name;
         // const value = event.target.value;
@@ -25,7 +25,10 @@ class AgeGuesser extends React.Component {
             clearTimeout(this.timeout);
         }
 
-        this.timeout = setTimeout((name) => {
+
+        this.timeout = setTimeout(() => {
+            // note: we use e.target.value instead of this.state.nameOfPerson
+            // why? becase setState is async, and we can't assume it has been updated when this runs
             axios.get(`https://api.agify.io/?name=${e.target.value}`)
                 .then(resp => this.setState({ age: resp.data.age}))
                 .catch(err => {
@@ -46,8 +49,6 @@ class AgeGuesser extends React.Component {
                     name="nameOfPerson"
                     autoComplete='off'
                     placeholder='Enter Name...'
-                    defaultValue={nameOfPerson}
-                    // trim input value to remove extra spaces
                     onChange={this.handleNameChange}
                     />
                 {/* React will render any type string or num so && here won't work */}
